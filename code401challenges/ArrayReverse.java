293721fa6cfc101f67b9bf2631e7b4c642b83863
MarishaHoza <marisha.hoza@gmail.com>
2019-08-05 18:11:44 -0700

    reverse method

--- code401challenges/ArrayReverse.java
+++ code401challenges/ArrayReverse.java
@@ -1,5 +1,24 @@
+// import arrays so that we can print them nicely
+import java.util.Arrays;
+
 public class ArrayReverse {
   public static void main(String[] args) {
     System.out.println("Hello World");
+
+    // call method here
+    int[] toBeReversed = new int[]{1,2,3,4,5};
+
+    System.out.println(Arrays.toString(reverseArray(toBeReversed)));
+  }
+
+  public static int[] reverseArray(int[] arr) {
+    int midpoint = arr.length / 2;
+    for (int i = 0; i < midpoint; i++){
+      int temp = arr[i];
+      int mirrorIndex = arr.length - 1 - i;
+      arr[i] = arr[mirrorIndex];
+      arr[mirrorIndex] = temp;
+    }
+    return arr;
   }
 }
