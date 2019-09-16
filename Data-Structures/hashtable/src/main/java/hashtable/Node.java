120153722d0a0495bb1c837927bb3461c5ba4e5c
MarishaHoza <marisha.hoza@gmail.com>
2019-09-15 18:21:01 -0700

    hashtable implementation

---
+++ Data-Structures/hashtable/src/main/java/hashtable/Node.java
@@ -0,0 +1,31 @@
+package hashtable;
+
+public class Node<K, V> {
+    K key;
+    V value;
+
+    public Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return this.key + ": " + this.value;
+    }
+}
